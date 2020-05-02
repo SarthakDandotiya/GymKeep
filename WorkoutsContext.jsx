c22969bcdbd3560f9bc4512975b7544c24c26f0a
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2020-05-02 12:37:26 +0530

    Update Persistence & minor Validation

--- WorkoutsContext.jsx
+++ WorkoutsContext.jsx
@@ -1,51 +1,26 @@
-import React, { useState, createContext, useEffect, useRef } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import { AsyncStorage } from "react-native";
 
 export const WorkoutsContext = createContext();
 
 export const WorkoutsProvider = (props) => {
-    const isInitialMount = useRef(true);
-    const [workouts, setWorkouts] = useState([]);
+  const [workouts, setWorkouts] = useState([]);
 
-    const getLocalWorkouts = async () => {
-        await AsyncStorage.getItem("gymKeepWorkouts")
-            .then((value) => {
-                // console.log("From Promise:\n\n", value);
-                if (typeof value != null) {
-                    setWorkouts(JSON.parse(value));
-                }
-            })
-            .done();
-    };
+  const setLocalWorkouts = async () => {
+    try {
+      await AsyncStorage.setItem("GYMKEEP", JSON.stringify(workouts));
+    } catch (error) {
+      console.error("Error Setting", error);
+    }
+  };
 
-    const setLocalWorkouts = async () => {
-        try {
-            await AsyncStorage.setItem(
-                "gymKeepWorkouts",
-                JSON.stringify(workouts)
-            );
-            // console.log(
-            //     "UPDATE:\n\n",
-            //     await AsyncStorage.getItem("gymKeepWorkouts")
-            // );
-        } catch (error) {
-            console.error("Error Setting", error);
-        }
-        // console.log("Done Setting.");
-    };
+  useEffect(() => {
+    setLocalWorkouts();
+  }, [workouts]);
 
-    useEffect(() => {
-        if (isInitialMount.current) {
-            isInitialMount.current = false;
-            getLocalWorkouts();
-        } else {
-            setLocalWorkouts();
-        }
-    }, [workouts]);
-
-    return (
-        <WorkoutsContext.Provider value={[workouts, setWorkouts]}>
-            {props.children}
-        </WorkoutsContext.Provider>
-    );
+  return (
+    <WorkoutsContext.Provider value={[workouts, setWorkouts]}>
+      {props.children}
+    </WorkoutsContext.Provider>
+  );
 };
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,14 +1,14 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import {
-    Text,
-    View,
-    StatusBar,
-    Image,
-    TextInput,
-    TouchableWithoutFeedback,
-    Keyboard,
-    Alert,
-    AsyncStorage,
+  Text,
+  View,
+  StatusBar,
+  Image,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+  AsyncStorage,
 } from "react-native";
 import { globalStyles } from "../styles/global";
 import { TouchableHighlight, ScrollView } from "react-native-gesture-handler";
@@ -17,58 +17,74 @@ import TouchableWorkoutListItem from "../shared/workoutItem";
 import { WorkoutsContext } from "../WorkoutsContext";
 
 export default function Home({ navigation }) {
-    let syncFlag = false;
+  let syncFlag = false;
 
-    const [workouts, setWorkouts] = useContext(WorkoutsContext);
-    const [workoutName, setWorkoutName] = useState("");
+  const isInitialMount = useRef(true);
+  const [workouts, setWorkouts] = useContext(WorkoutsContext);
+  const [workoutName, setWorkoutName] = useState("");
 
-    const longPressHandler = (timeStamp) => {
-        setWorkouts((oldWorkouts) => {
-            return oldWorkouts.filter(
-                (workout) => workout.startedAt != timeStamp
-            );
-        });
-    };
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+      initialPopulate();
+    }
+  }, []);
 
-    const textChangeHandler = (text) => {
-        setWorkoutName(text);
-    };
-
-    const beginWorkoutHandler = () => {
-        if (workoutName.length > 2) {
-            if (syncFlag === false) {
-                syncFlag = true;
-                const startedAt = Date.parse(new Date().toJSON());
-                navigation.navigate("ActiveWorkout", {
-                    name: workoutName,
-                    startedAt,
-                });
-                setTimeout(() => {
-                    syncFlag = false;
-                }, 2000);
-                setWorkoutName("");
-                clearInterval();
-            }
+  const initialPopulate = () => {
+    AsyncStorage.getItem("GYMKEEP")
+      .then((data) => {
+        if (data !== null) {
+          // if(data !== null)
+          console.log("From Promise:\n\n", data);
+          setWorkouts(JSON.parse(data));
         } else {
-            Alert.alert(
-                "Oops!",
-                "Workout name must be atleast 3 characters long..."
-            );
+          setWorkouts([]);
         }
-    };
+      })
+      .done(console.log("Populated Initially"));
+  };
 
-    return (
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-            {}
-            <View style={globalStyles.container}>
-                <StatusBar backgroundColor='#000000' barStyle='light-content' />
-                {/* Header */}
-                <View style={globalStyles.alternateHeader}>
-                    <View>
-                        <Text style={globalStyles.brand}>Gym</Text>
-                        <Text style={globalStyles.brand}>Keep</Text>
-                    </View>
-                    {/* <TouchableHighlight
+  const longPressHandler = (timeStamp) => {
+    setWorkouts((oldWorkouts) => {
+      return oldWorkouts.filter((workout) => workout.startedAt != timeStamp);
+    });
+  };
+
+  const textChangeHandler = (text) => {
+    setWorkoutName(text);
+  };
+
+  const beginWorkoutHandler = () => {
+    if (workoutName.trim().length > 2) {
+      if (syncFlag === false) {
+        syncFlag = true;
+        const startedAt = Date.parse(new Date().toJSON());
+        navigation.navigate("ActiveWorkout", {
+          name: workoutName.trim(),
+          startedAt,
+        });
+        setTimeout(() => {
+          syncFlag = false;
+        }, 2000);
+        setWorkoutName("");
+        clearInterval();
+      }
+    } else {
+      Alert.alert("Oops!", "Workout name must be atleast 3 characters long...");
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={globalStyles.container}>
+        <StatusBar backgroundColor="#000000" barStyle="light-content" />
+        {/* Header */}
+        <View style={globalStyles.alternateHeader}>
+          <View>
+            <Text style={globalStyles.brand}>Gym</Text>
+            <Text style={globalStyles.brand}>Keep</Text>
+          </View>
+          {/* <TouchableHighlight
                         onPress={() => navigation.navigate("Options")}
                     >
                         <Image
@@ -76,87 +92,78 @@ export default function Home({ navigation }) {
                             style={globalStyles.dp}
                         />
                     </TouchableHighlight> */}
-                </View>
-                {/* Image */}
-                <Image
-                    source={require("../assets/bicep.png")}
-                    style={globalStyles.bicep}
-                />
-                {/* Content */}
-                <View>
-                    {/* New Workout Section */}
-                    <View style={{ marginBottom: 32 }}>
-                        <Text style={globalStyles.heading}>New</Text>
-                        <TextInput
-                            style={globalStyles.textInput}
-                            onChangeText={(text) => textChangeHandler(text)}
-                            value={workoutName}
-                            placeholder='Workout Name'
-                        />
-                        <FlatButton
-                            text='Begin'
-                            onPress={beginWorkoutHandler}
-                        />
-                    </View>
-                    {/* Previous Section */}
-                    <View>
-                        <View
-                            style={{
-                                flex: 0,
-                                flexDirection: "row",
-                                justifyContent: "space-between",
-                                alignItems: "center",
-                            }}
-                        >
-                            <Text style={globalStyles.heading}>Previous</Text>
-                            <TouchableHighlight
-                                onPress={() =>
-                                    navigation.navigate("AllWorkouts")
-                                }
-                            >
-                                <Text style={globalStyles.textGreen}>
-                                    View All
-                                </Text>
-                            </TouchableHighlight>
-                        </View>
-                        {/* Short List */}
-                        <ScrollView style={{ minHeight: 76, maxHeight: 232 }}>
-                            {workouts.length < 3 ? (
-                                <View
-                                    style={{
-                                        flex: 1,
-                                        flexDirection: "column",
-                                        justifyContent: "center",
-                                        alignItems: "center",
-                                        height: 232,
-                                        backgroundColor: "#1a1a1a",
-                                    }}
-                                >
-                                    <Text style={globalStyles.text}>
-                                        You need to have atleast 3 workouts
-                                    </Text>
-                                    <Text style={globalStyles.text555}>
-                                        Get going!
-                                    </Text>
-                                </View>
-                            ) : (
-                                workouts.slice(0, 3).map((item) => {
-                                    return (
-                                        <TouchableWorkoutListItem
-                                            key={item.startedAt}
-                                            name={item.name}
-                                            startedAt={item.startedAt}
-                                            exercises={item.exercises}
-                                            navigation={navigation}
-                                            longPressHandler={longPressHandler}
-                                        />
-                                    );
-                                })
-                            )}
-                        </ScrollView>
-                    </View>
-                </View>
+        </View>
+        {/* Image */}
+        <Image
+          source={require("../assets/bicep.png")}
+          style={globalStyles.bicep}
+        />
+        {/* Content */}
+        <View>
+          {/* New Workout Section */}
+          <View style={{ marginBottom: 32 }}>
+            <Text style={globalStyles.heading}>New</Text>
+            <TextInput
+              style={globalStyles.textInput}
+              onChangeText={(text) => textChangeHandler(text)}
+              value={workoutName}
+              placeholder="Workout Name"
+            />
+            <FlatButton text="Begin" onPress={beginWorkoutHandler} />
+          </View>
+          {/* Previous Section */}
+          <View>
+            <View
+              style={{
+                flex: 0,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Text style={globalStyles.heading}>Previous</Text>
+              <TouchableHighlight
+                onPress={() => navigation.navigate("AllWorkouts")}
+              >
+                <Text style={globalStyles.textGreen}>View All</Text>
+              </TouchableHighlight>
             </View>
-        </TouchableWithoutFeedback>
-    );
+            {/* Short List */}
+            <ScrollView style={{ minHeight: 76, maxHeight: 232 }}>
+              {workouts.length < 3 ? (
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: 232,
+                    backgroundColor: "#1a1a1a",
+                  }}
+                >
+                  <Text style={globalStyles.text}>
+                    You need to have atleast 3 workouts
+                  </Text>
+                  <Text style={globalStyles.text555}>Get going!</Text>
+                </View>
+              ) : (
+                workouts.slice(0, 3).map((item) => {
+                  return (
+                    <TouchableWorkoutListItem
+                      key={item.startedAt}
+                      name={item.name}
+                      startedAt={item.startedAt}
+                      exercises={item.exercises}
+                      navigation={navigation}
+                      longPressHandler={longPressHandler}
+                    />
+                  );
+                })
+              )}
+            </ScrollView>
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
